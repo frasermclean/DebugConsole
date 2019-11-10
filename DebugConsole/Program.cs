989bbff0b12c4c5a7d65dd4b9f9a4e916bed4520
Fraser McLean <fraser.mclean@atcontrols.com.au>
2019-11-10 11:06:20 +1100

    Add project files.

---
+++ DebugConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DebugConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Your name is: " + name);
+        }
+    }
+}
