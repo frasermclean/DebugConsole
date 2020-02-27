18af11e74302145e5c61212a48009777294ee43e
Fraser McLean <dev@frasermclean.com>
2020-02-27 11:47:50 +1100

    Started converting listener class to be UDP based

--- DebugConsole/Listener.cs
+++ DebugConsole/Listener.cs
@@ -1,93 +1,41 @@
 ﻿using System;
 using System.Net;
-using System.Net.Sockets;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
 
 namespace DebugConsole
 {
     class Listener
     {
         public const int PortDefault = 5000;
-        const int BufferSize = 4096;
+        private readonly int port;
 
         public Listener(int port)
         {
-            TcpListener server = null;
+            this.port = port;
+            Thread thread = new Thread(new ThreadStart(WorkerMethod));
+        }
+
+        void WorkerMethod()
+        {
+            // create udp client for reading incoming data
+            UdpClient client = new UdpClient(port);
+
+            // remote end point to record ip address and port number of sender
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
             try
-            {                
-                IPAddress allInterfaces = IPAddress.Any;
-
-                // start listening for client requests.
-                server = new TcpListener(allInterfaces, port);
-                server.Start();
-
-                // buffer for storing incoming data
-                byte[] receiveBuffer = new byte[BufferSize];
-
-                // main loop
-                while (true)
-                {
-                    Console.WriteLine("Listening for incoming connections on port: " + port);
-                    TcpClient client = server.AcceptTcpClient(); // blocking call
-                    Console.WriteLine("Accepted connection from: " + client.Client.RemoteEndPoint.ToString());
-
-                    // Get a stream object for reading and writing
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    // Loop to receive all the data sent by the client.
-                    while ((i = stream.Read(receiveBuffer, 0, receiveBuffer.Length)) != 0)
-                    {
-                        // get received string
-                        string receivedString = System.Text.Encoding.ASCII.GetString(receiveBuffer, 0, i).Trim();
-                        //Console.WriteLine(receivedString);
-
-                        foreach (string jsonString in GetJsonStrings(receivedString))
-                        {
-                            Message receivedMessage = Message.DecodeFromJson(jsonString);
-                            if (receivedMessage != null)
-                            {
-                                // set foreground colour based on message type
-                                switch (receivedMessage.MessageType)
-                                {
-                                    case MessageType.None: Console.ForegroundColor = ConsoleColor.White; break;
-                                    case MessageType.Info: Console.ForegroundColor = ConsoleColor.Green; break;
-                                    case MessageType.Warning: Console.ForegroundColor = ConsoleColor.Yellow; break;
-                                    case MessageType.Error: Console.ForegroundColor = ConsoleColor.Red; break;
-                                    case MessageType.Exception: Console.ForegroundColor = ConsoleColor.Magenta; break;
-                                }
-
-                                Console.WriteLine("[{0}] {1}", receivedMessage.ComponentName, receivedMessage.MessageText);
-                                Console.ForegroundColor = ConsoleColor.White; // reset foreground color to default
-                            }
-                        }
-
-                        //// generate response
-                        //DebugConsoleMessage responseMessage = new DebugConsoleMessage(MessageType.None, "DebugConsole", "OK");
-                        //string responseString = DebugConsoleMessage.EncodeToJson(responseMessage);
-
-                        //byte[] responseBytes = System.Text.Encoding.ASCII.GetBytes(responseString);
-                        //stream.Write(responseBytes, 0, responseBytes.Length);
-                        //Console.WriteLine("Sent: {0}", responseString);
-                    }
-
-                    // Shutdown and end connection
-                    client.Close();
-                }
-            }
-            catch (SocketException e)
             {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            finally
-            {
-                // Stop listening for new clients.
-                server.Stop();
-            }
+                // blocks until a message returns on this socket from a remote host.
+                byte[] receiveBytes = client.Receive(ref remoteEndPoint);
 
-            Console.WriteLine("\nHit enter to continue...");
-            Console.Read();
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+            }
+            catch (Exception e)
+            {
+            }
         }
 
         List<string> GetJsonStrings(string receivedString)
@@ -99,7 +47,7 @@ namespace DebugConsole
 
             int startPos, endPos;
             startPos = 0;
-            while(startPos < receivedString.Length)
+            while (startPos < receivedString.Length)
             {
                 startPos = receivedString.IndexOf("{", startPos);
                 endPos = receivedString.IndexOf("}", startPos + 1);
@@ -110,7 +58,6 @@ namespace DebugConsole
                     break;
                 }
                 int length = endPos - startPos + 1;
-                //Console.WriteLine("GetJsonStrings() StartPos: {0}, EndPos: {1}, Length: {2}", startPos, endPos, length);
                 string jsonString = receivedString.Substring(startPos, length);
                 list.Add(jsonString);
                 startPos = endPos + 1;
